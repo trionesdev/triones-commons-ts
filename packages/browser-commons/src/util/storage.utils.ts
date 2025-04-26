d9cfe9b72cb633a05a665ae08aa074ab1e557640
落雁沙 <fengxiaotx@163.com>
2025-04-26 13:09:01 +0800

    feat:storage

--- packages/browser-commons/src/util/storage.utils.ts
+++ packages/browser-commons/src/util/storage.utils.ts
@@ -3,26 +3,32 @@ import Cookies from "js-cookie";
 const USER_TOKEN = "TrionesUserToken";
 
 export class StorageUtils {
-  static setTrionesUserToken = (token: string) => {
-    sessionStorage.setItem(USER_TOKEN, token);
-    Cookies.set(USER_TOKEN, token);
-  };
+    static setTrionesUserToken = (token: string) => {
+        sessionStorage.setItem(USER_TOKEN, token);
+        Cookies.set(USER_TOKEN, token);
+    };
 
-  static getTrionesUserToken = () => {
-    return sessionStorage.getItem(USER_TOKEN) || Cookies.get(USER_TOKEN);
-  };
+    static getTrionesUserToken = () => {
+        return sessionStorage.getItem(USER_TOKEN) || Cookies.get(USER_TOKEN);
+    };
 
-  static removeTenantUserToken = () => {
-    sessionStorage.removeItem(USER_TOKEN);
-    Cookies.remove(USER_TOKEN);
-  };
+    static removeTenantUserToken = () => {
+        sessionStorage.removeItem(USER_TOKEN);
+        Cookies.remove(USER_TOKEN);
+    };
 
-  static setToken = (key: string, token: string) => {
-    sessionStorage.setItem(key, token);
-    Cookies.set(key, token);
-  };
+    static setItem = (key: string, token: string) => {
+        sessionStorage.setItem(key, token);
+        Cookies.set(key, token);
+    };
 
-  static getToken = (key: string): string | undefined => {
-    return sessionStorage.getItem(key) || Cookies.get(key);
-  };
+    static getItem = (key: string): string | undefined => {
+        return sessionStorage.getItem(key) || Cookies.get(key);
+    };
+
+    static removeItem = (key: string) => {
+        sessionStorage.removeItem(key);
+        Cookies.remove(key);
+    };
+
 }
